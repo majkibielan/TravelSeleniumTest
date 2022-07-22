4779bfa6dee898f9001bec5166988a1e04edb181
majkibielan <majkibielan@gmail.com>
2022-07-22 13:51:48 +0200

    228 - 230

--- src/test/java/HotelFind.java
+++ src/test/java/HotelFind.java
@@ -2,12 +2,16 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class HotelFind {
 
@@ -27,5 +31,19 @@ public class HotelFind {
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@class='select2-match' and text()='Dubai']"))).click();
         driver.findElement(By.name("checkin")).sendKeys("25/07/2022");
         driver.findElement(By.name("checkout")).sendKeys("30/07/2022");
+        driver.findElement(By.id("travellersInput")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("adultPlusBtn"))).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("childPlusBtn"))).click();
+        driver.findElement(By.xpath("//button[text()=' Search']")).click();
+
+        List<String> hotelNames = driver.findElements(By.xpath("//h4//b"))
+                                                        .stream()
+                                                        .map(el -> el.getAttribute("textContent"))
+                                                        .collect(Collectors.toList());
+
+        Assert.assertEquals("Jumeirah Beach Hotel", hotelNames.get(0), "Jumeirah Beach Hotel nie znaleziony");
+        Assert.assertEquals("Oasis Beach Tower", hotelNames.get(1), "Oasis Beach Tower nie znaleziony");
+        Assert.assertEquals("Rose Rayhaan Rotana", hotelNames.get(2), "Rose Rayhaan Rotana nie znaleziony");
+        Assert.assertEquals("Hyatt Regency Perth", hotelNames.get(3), "Hyatt Regency Perth nie znaleziony");
     }
 }
